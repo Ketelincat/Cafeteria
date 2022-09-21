cca15b8f9939a731fbda642ffc388718f8cd0d9a
Laboratorio-Info <ketelinoliveira23@gmail.com>
2022-09-20 22:10:02 -0300

    add adapter

--- app/src/main/java/br/com/etecia/myapplication/CoffeeFragment.java
+++ app/src/main/java/br/com/etecia/myapplication/CoffeeFragment.java
@@ -3,6 +3,7 @@ package br.com.etecia.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 
 public class CoffeeFragment extends Fragment {
 
+    RecyclerView rvCoffee;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,5 +20,6 @@ public class CoffeeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_coffee, container, false);
 
-        return view;}
+        return view;
+    }
 }
