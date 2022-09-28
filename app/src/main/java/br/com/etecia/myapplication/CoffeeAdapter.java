b29660482556c8ec85ee5988c53d330511d7f1a8
ketelincat <ketelinoliveira23@gmail.com>
2022-09-27 21:39:16 -0300

    atualizado fotos

--- app/src/main/java/br/com/etecia/myapplication/CoffeeAdapter.java
+++ app/src/main/java/br/com/etecia/myapplication/CoffeeAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -35,21 +36,30 @@ public class CoffeeAdapter extends RecyclerView.Adapter<CoffeeAdapter.MyViewHold
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.txtTitulo.setText(mData.get(position).getTitulo());
+        holder.txtDescricao.setText(mData.get(position).getDescricao());
+        holder.txtNota.setText(mData.get(position).getNota());
+        holder.imgCafe.setImageResource(mData.get(position).getImagem());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return mData.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
         TextView txtTitulo, txtDescricao, txtNota;
-
+        ImageView imgCafe;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            txtTitulo = itemView.findViewById(R.id.txttituloCafe);
+            txtDescricao = itemView.findViewById(R.id.txtFrasecafe);
+            txtNota = itemView.findViewById(R.id.txtNotaCafe);
+            imgCafe = itemView.findViewById(R.id.imgCafe);
 
         }
     }
--- app/src/main/java/br/com/etecia/myapplication/CoffeeFragment.java
+++ app/src/main/java/br/com/etecia/myapplication/CoffeeFragment.java
@@ -28,14 +28,14 @@ public class CoffeeFragment extends Fragment {
 
         lstCoffee = new ArrayList<>();
 
-       lstCoffee.add(new Coffee("Café", "CAfe quentinho", "9.5", R.drawable.cafe));
-       lstCoffee.add(new Coffee("Café", "CAfe quentinho", "9.5", R.drawable.cafe));
-       lstCoffee.add(new Coffee("Café", "CAfe quentinho", "9.5", R.drawable.cafe));
-       lstCoffee.add(new Coffee("Café", "CAfe quentinho", "9.5", R.drawable.cafe));
-       lstCoffee.add(new Coffee("Café", "CAfe quentinho", "9.5", R.drawable.cafe));
-       lstCoffee.add(new Coffee("Café", "CAfe quentinho", "9.5", R.drawable.cafe));
-       lstCoffee.add(new Coffee("Café", "CAfe quentinho", "9.5", R.drawable.cafe));
-       lstCoffee.add(new Coffee("Café", "CAfe quentinho", "9.5", R.drawable.cafe));
+       lstCoffee.add(new Coffee("Café", "Negao do jeito que tu gosta", "9.5", R.drawable.cafe));
+       lstCoffee.add(new Coffee("Café com leite", "Seu café palmiteiro", "10.0", R.drawable.cafe_com_leite));
+       lstCoffee.add(new Coffee("Café Expresso", "Seu café mais rápido que um trem", "9.7", R.drawable.cafe_expresso));
+       lstCoffee.add(new Coffee("Capuccino", "isso aqui foi misturado com Toddy", "7.0", R.drawable.capuccino));
+       lstCoffee.add(new Coffee("Irish Coffee", "Fique bebado no seu trabalho", "10.0", R.drawable.irish_coffee));
+       lstCoffee.add(new Coffee("Machiatto", "Café light-skin", "8.3", R.drawable.machiatto));
+       lstCoffee.add(new Coffee("Mocha", "Cafe que não é café", "7.0", R.drawable.mocha));
+       lstCoffee.add(new Coffee("Latte", "AU AU", "9.5", R.drawable.latte));
 
         lstlistacafe = view.findViewById(R.id.lstlistacafe);
 
